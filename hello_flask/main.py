de8f7194643f9e894f7194cf435a91cf31f8837f
SingleUnitBox <cezaryzukowski90@gmail.com>
2022-10-25 23:16:31 +0100

    Add files via upload

--- hello_flask/main.py
+++ hello_flask/main.py
@@ -1,1 +1,45 @@
+from flask import Flask
+import random
 
+
+# app = Flask(__name__)
+#
+# print(random.__name__)
+# print(__name__)
+
+
+# def outer_function():
+#     print("I'm outer")
+#
+#     def nested_function():
+#         print("I'm inner")
+#
+#     return nested_function
+#
+# inner_function = outer_function()
+# inner_function()
+
+import time
+
+
+def speed_calc_decorator(function):
+    def wrapper_function():
+        current_time = time.time()
+        function()
+        end_time = time.time()
+        return end_time - current_time
+    return wrapper_function
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(10000000):
+        i * i
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(100000000):
+        i * i
+
+print(fast_function())
+print(slow_function())
