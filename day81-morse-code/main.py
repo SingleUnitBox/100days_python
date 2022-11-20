3ddc896504be2e151aed8bfba69e785c8304c111
SingleUnitBox <cezaryzukowski90@gmail.com>
2022-11-20 19:50:07 +0000

    Add files via upload

--- day81-morse-code/main.py
+++ day81-morse-code/main.py
@@ -1,1 +1,38 @@
 
+morse = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--.."
+}
+word_morse = []
+word = input("Your word: ")
+for char in word:
+    char_upper = char.upper()
+    if char_upper in morse:
+        morse_char = morse[char_upper]
+        word_morse.append(morse_char)
+print(word_morse)
+
