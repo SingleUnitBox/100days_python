accc5e80bae5a8290ae0adc298410b11986f125c
SingleUnitBox <cezaryzukowski90@gmail.com>
2022-11-22 20:00:36 +0000

    Add files via upload

--- day85/main.py
+++ day85/main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import random
+import time
 
 word_list = ["lumber", "talented", "successful", "worthless", "arrange", "luxuriant", "unadvised", "stay", "rings",
              "proud", "freezing", "pastoral", "wrathful", "faded", "melted", "zippy", "harm", "nutty", "jump",
@@ -9,7 +10,10 @@ word_list = ["lumber", "talented", "successful", "worthless", "arrange", "luxuri
 counter = 0
 i = 0
 words_per_minute = 0
-correct_words_per_minute = 0
+correct_words = 0
+timer = 60
+
+
 
 
 random.shuffle(word_list)
@@ -26,8 +30,12 @@ field.configure(state="disabled")
 # field.tag_add("typed_word", f"1.{counter}", f"1.{counter+len(word_list[i])}")
 # field.tag_config("typed_word", background="grey")
 
-label_timer = tk.Label(text="Timer", font=("Arial", 20, "bold"))
-label_timer.grid(column=0, row=2)
+label_words = tk.Label(text=f"correct words: {correct_words}", font=("Arial", 20, "bold"))
+label_words.grid(column=0, row=2)
+
+label_timer = tk.Label(text=f"60", font=("Arial", 20, "bold"))
+label_timer.grid(column=0, row=3)
+
 
 input = tk.Entry(width=50, justify="center", font=("Arial", 20, "bold"))
 input.grid(column=0, row=1)
@@ -36,38 +44,54 @@ def my_function(self):
     global i
     global counter
     global words_per_minute
-    global correct_words_per_minute
+    global correct_words
     input_word = str(input.get()).strip()
-    if input_word == word_list[i]:
-        # print(input_word)
-        # print(word_list[i])
-        #print(f"counter: {counter} word_len: {len(word_list[i])} i: {i}")
-        field.tag_add(f"typed_word{i}", f"1.{counter}", f"1.{counter+len(word_list[i])}")
-        field.tag_config(f"typed_word{i}", background="green")
-        counter += len(word_list[i])
-        counter +=1
-        # print(f"counter: {counter} word_len: {len(word_list[i])}, i: {i}")
-        # field.tag_add("next_word", f"1.{counter}", f"1.{counter+len(word_list[i+1])}")
-        # field.tag_config("next_word", background="gray")
-        input.delete(0, "end")
-        i += 1
-        words_per_minute += 1
-        correct_words_per_minute += 1
+    if i == len(word_list):
+        print("you are done")
     else:
-        #print(f"counter: {counter} word_len: {len(word_list[i])} i: {i}")
-        field.tag_add(f"typed_word{i}", f"1.{counter}", f"1.{counter + len(word_list[i])}")
-        field.tag_config(f"typed_word{i}", background="red")
-        counter += len(word_list[i])
-        counter += 1
-        # print(f"counter: {counter} word_len: {len(word_list[i])}, i: {i}")
-        # field.tag_add("next_word", f"1.{counter}", f"1.{counter+len(word_list[i+1])}")
-        # field.tag_config("next_word", background="gray")
-        input.delete(0, "end")
-        i += 1
-        words_per_minute += 1
+        if input_word == word_list[i]:
+            # print(input_word)
+            # print(word_list[i])
+            #print(f"counter: {counter} word_len: {len(word_list[i])} i: {i}")
+            field.tag_add(f"typed_word{i}", f"1.{counter}", f"1.{counter+len(word_list[i])}")
+            field.tag_config(f"typed_word{i}", background="green")
+            counter += len(word_list[i])
+            counter +=1
+            # print(f"counter: {counter} word_len: {len(word_list[i])}, i: {i}")
+            # field.tag_add("next_word", f"1.{counter}", f"1.{counter+len(word_list[i+1])}")
+            # field.tag_config("next_word", background="gray")
+            input.delete(0, "end")
+            i += 1
+            words_per_minute += 1
+            correct_words += 1
+            label_words.config(text=f"correct words: {correct_words}")
+        else:
+            #print(f"counter: {counter} word_len: {len(word_list[i])} i: {i}")
+            field.tag_add(f"typed_word{i}", f"1.{counter}", f"1.{counter + len(word_list[i])}")
+            field.tag_config(f"typed_word{i}", background="red")
+            counter += len(word_list[i])
+            counter += 1
+            # print(f"counter: {counter} word_len: {len(word_list[i])}, i: {i}")
+            # field.tag_add("next_word", f"1.{counter}", f"1.{counter+len(word_list[i+1])}")
+            # field.tag_config("next_word", background="gray")
+            input.delete(0, "end")
+            i += 1
+            words_per_minute += 1
 
 
 root.bind("<space>", my_function)
+def count_down(count):
+    if count >= 1:
+        root.after(1000, count_down, count - 1)
+        label_timer.config(text=f"{count}")
+    else:
+        label_timer.config(text=f"0")
+        words_per_minute_label = tk.Label(text=f"You have typed {correct_words} per minute", font=("Arial", 20, "bold"))
+        words_per_minute_label.grid(column=0, row=4)
+
+
+timer = count_down(60)
+
 
 root.mainloop()
 
