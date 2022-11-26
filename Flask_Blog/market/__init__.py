e095347f470a1c10517b2ad7d43a477b6bc4d6c5
SingleUnitBox <cezaryzukowski90@gmail.com>
2022-11-26 15:39:23 +0000

    Add files via upload

--- Flask_Blog/market/__init__.py
+++ Flask_Blog/market/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'b5a1d31cf8a4256062bab448'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
